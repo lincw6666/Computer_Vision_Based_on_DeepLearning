b09d2e505c8c826fb62ad20bbbc8f57e462cc125
davidlin <aunbdaunbd@gmail.com>
2019-12-16 23:24:46 +0800

    < HW04 > Add train.py.
    # Description:
    It'll train the mini dataset of HW04.

---
+++ HW04/train.py
@@ -0,0 +1,40 @@
+# Some basic setup
+# Setup detectron2 logger
+import torch
+import torchvision
+import detectron2
+from detectron2.utils.logger import setup_logger
+
+# import some common libraries
+import numpy as np
+import os
+
+# import some common detectron2 utilities
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+
+# Packages for dataset.
+from detectron2.data.datasets import register_coco_instances
+
+
+# Register the dataset.
+setup_logger()
+register_coco_instances(
+    "train_dataset", {}, "data/pascal_train.json", "data/train_images")
+register_coco_instances(
+    "test_dataset", {}, "data/test.json", "data/test_images")
+
+# Configuration.
+cfg = get_cfg()
+# Path to the config file.
+cfg.merge_from_file(
+    './detectron2/configs/COCO-InstanceSegmentation/' +
+    'mask_rcnn_R_50_FPN_decay_0.01_momentum_0.9.yaml')
+cfg.SOLVER.MAX_ITER = 100
+cfg.OUTPUT_DIR = './train_logs/'
+
+# Start training.
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
